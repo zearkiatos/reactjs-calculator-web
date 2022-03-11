6d5e773065fda3a71e9934b9d9b33e4f57534327
zearkiatos <caprilespe@outlook.com>
2022-03-11 15:52:07 -0300

    SOLID Pattern

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,34 @@ const App = () => {
   console.log("Application render");
   return (
     <main className="react-calculator">
-      Web Calculator 🧮
+      <div className="result"></div>
+      <div className="numbers">
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>0</button>
+      </div>
+      <div className="functions">
+        <button>
+          C
+        </button>
+        <button>
+          ⬅
+        </button>
+      </div>
+      <div className="math-operations">
+        <button>➕</button>
+        <button>➖</button>
+        <button>✖️</button>
+        <button>➗</button>
+        <button>=</button>
+      </div>
     </main>
   );
 };

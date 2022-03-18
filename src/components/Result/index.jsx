de1624344190ff184a8b54a0178724134426fb82
zearkiatos <caprilespe@outlook.com>
2022-03-18 18:29:29 -0300

    Utilization the various properties

--- src/components/Result/index.jsx
+++ src/components/Result/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
-const Result = ({ value }) => {
+const Result = ({ value, text }) => {
     console.log("Renderization result");
     console.log(value);
+    console.log(text);
     return (
         <div className="result">
             <span>0</span>

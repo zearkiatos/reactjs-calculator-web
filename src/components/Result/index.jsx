b5b3d163813311bc8f619822ae6f5bbfe81548cf
zearkiatos <caprilespe@outlook.com>
2022-03-20 17:48:49 -0300

    Basic validation with PropTypes

--- src/components/Result/index.jsx
+++ src/components/Result/index.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Result = ({ value }) => (
   <div className="result">
     <span>{value}</span>
   </div>
 );
+
+Result.propTypes = {
+  value: PropTypes.number.isRequired
+};
 
 export default Result;

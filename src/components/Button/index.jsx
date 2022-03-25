726f82d3b2935b10f171a07e95e5c2f8e8260a84
zearkiatos <caprilespe@outlook.com>
2022-03-25 16:07:35 -0300

    PropTypes in Button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
 
 const Button = ({ type, text, clickHandler }) => {
-    return (
-        <button className={type} onClick={() => clickHandler(text)}>
-            <span>{text}</span>
-        </button>
-    );
+  return (
+    <button className={type} onClick={() => clickHandler(text)}>
+      <span>{text}</span>
+    </button>
+  );
+};
+
+Button.propTypes = {
+  type: PropTypes.string,
+  text: PropTypes.string.isRequired,
+  clickHandler: PropTypes.func.isRequired
 };
 
 export default Button;

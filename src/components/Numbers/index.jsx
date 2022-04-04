e139eb8b1c38031a3bc43d601e1b73fab17ddefe
zearkiatos <caprilespe@outlook.com>
2022-04-04 19:26:15 -0400

    DRY applied to renderButtons function

--- src/components/Numbers/index.jsx
+++ src/components/Numbers/index.jsx
@@ -2,18 +2,13 @@ import React from "react";
 import PropTypes from 'prop-types'
 import Button from "../Button";
 
+const renderButtons = onClickNumber => {
+  //<Button text="1" clickHandler={onClickNumber} /> 
+};
+
 const Numbers = ({ onClickNumber }) => (
   <div className="numbers">
-    <Button text="1" clickHandler={onClickNumber} />
-    <Button text="2" clickHandler={onClickNumber} />
-    <Button text="3" clickHandler={onClickNumber} />
-    <Button text="4" clickHandler={onClickNumber} />
-    <Button text="5" clickHandler={onClickNumber} />
-    <Button text="6" clickHandler={onClickNumber} />
-    <Button text="7" clickHandler={onClickNumber} />
-    <Button text="8" clickHandler={onClickNumber} />
-    <Button text="9" clickHandler={onClickNumber} />
-    <Button text="0" clickHandler={onClickNumber} />
+    {renderButtons(onClickNumber)}
   </div>
 );
 

f8c3cb402eb7466e15f7728b95e24af619135e83
Pedro Capriles <pedrocapriles@Pedros-MacBook-Pro.local>
2022-04-08 18:41:13 -0400

    Using the property key

--- src/components/Numbers/index.jsx
+++ src/components/Numbers/index.jsx
@@ -4,7 +4,7 @@ import Button from "../Button";
 
 const renderButtons = onClickNumber => {
   const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-  return numbers.map(number => <Button text={number} clickHandler={onClickNumber} /> )
+  return numbers.map(number => <Button key={number} text={number.toString()} clickHandler={onClickNumber} />)
 };
 
 const Numbers = ({ onClickNumber }) => (

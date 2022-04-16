4b9874c7f017b3ed795cf81294452a561436b8a6
zearkiatos <caprilespe@outlook.com>
2022-04-15 21:16:02 -0400

    Adding app functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,30 +2,26 @@ import React, { useState } from "react";
 import Result from "./Result";
 import Numbers from "./Numbers";
 import MathOperations from "./MathOperations";
-import Functions from './Functions';
+import Functions from "./Functions";
 import "../styles/app.css";
 
 const App = () => {
   const [stack, setStack] = useState("");
-  const onClickNumber = (number) => setStack(number);
+  const onClickNumber = (number) => setStack(stack + number);
 
-  const onContentClear = () => console.log('Content Clear');
+  const onContentClear = () => console.log("Content Clear");
 
-  const onDelete = () => console.log('Delete');
+  const onDelete = () => console.log("Delete");
 
-  const onClickOperation = (operation) => console.log(`Operation: ${operation}`);
+  const onClickOperation = (operation) =>
+    console.log(`Operation: ${operation}`);
 
-  const onClickEqual = () => console.log('Equals');
+  const onClickEqual = () => console.log("Equals");
   return (
     <main className="react-calculator">
       <Result value={stack} />
-      <Numbers
-        onClickNumber={onClickNumber}
-      />
-      <Functions
-        onDelete={onDelete}
-        onContentClear={onContentClear}
-      />
+      <Numbers onClickNumber={onClickNumber} />
+      <Functions onDelete={onDelete} onContentClear={onContentClear} />
       <MathOperations
         onClickOperation={onClickOperation}
         onClickEqual={onClickEqual}
